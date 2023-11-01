da8bb4fb887a7b4d728b668efa2e0cef7ba2bf89
Jose Lucas <lucasnba2324@gmail.com>
2023-10-31 23:07:54 -0300

    creating app

--- src/App.js
+++ src/App.js
@@ -1,8 +1,13 @@
 import './App.css';
+import AppRouter from './AppRouter';
+import { BrowserRouter as Router } from 'react-router-dom';
 
 function App() {
   return (
     <div className="App">
+      <Router>
+        <AppRouter />
+      </Router>
     </div>
   );
 }
