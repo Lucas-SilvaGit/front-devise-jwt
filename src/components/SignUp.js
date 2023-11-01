2b53062c0ade2726eb422c15d0e750b3902daa65
Jose Lucas <lucasnba2324@gmail.com>
2023-10-31 23:08:18 -0300

    creating component Sign Up

---
+++ src/components/SignUp.js
@@ -0,0 +1,62 @@
+import React, { useState } from 'react';
+
+const Signup = () => {
+  const [formData, setFormData] = useState({
+    username: '',
+    email: '',
+    password: '',
+  });
+
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setFormData({
+      ...formData,
+      [name]: value,
+    });
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    // Aqui você pode adicionar o código para enviar os dados de registro para o servidor
+    // Por exemplo, você pode usar a função fetch() ou uma biblioteca para fazer uma solicitação POST para o servidor.
+    console.log(formData);
+  };
+
+  return (
+    <div className="container">
+      <h2>Signup</h2>
+      <form onSubmit={handleSubmit}>
+        <div className="form-group">
+          <label>Username</label>
+          <input
+            type="text"
+            name="username"
+            value={formData.username}
+            onChange={handleChange}
+          />
+        </div>
+        <div className="form-group">
+          <label>Email</label>
+          <input
+            type="email"
+            name="email"
+            value={formData.email}
+            onChange={handleChange}
+          />
+        </div>
+        <div className="form-group">
+          <label>Password</label>
+          <input
+            type="password"
+            name="password"
+            value={formData.password}
+            onChange={handleChange}
+          />
+        </div>
+        <button type="submit">Sign Up</button>
+      </form>
+    </div>
+  );
+};
+
+export default Signup;
