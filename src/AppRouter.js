305f4e6ba4b16b88b38f2d95b05071fbb876bc14
Jose Lucas <lucasnba2324@gmail.com>
2023-11-03 23:56:46 -0300

    adding routes accounts

--- src/AppRouter.js
+++ src/AppRouter.js
@@ -3,6 +3,7 @@ import { Routes, Route } from 'react-router-dom';
 import Home from './components/Home';
 import SignUp from './components/SignUp';
 import SignIn from './components/SignIn';
+import Account from './components/Account';
 
 
 const AppRouter = () => {
@@ -11,6 +12,7 @@ const AppRouter = () => {
       <Route path="/" element={<Home />} />
       <Route path="signup" element={<SignUp />} />
       <Route path="signin" element={<SignIn />} />
+      <Route path="account" element={<Account />} />
     </Routes>
   );
 };
